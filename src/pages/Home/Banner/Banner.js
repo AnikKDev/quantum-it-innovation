a24f1c6f89cc2e548db21b062371e7a1b9492770
AnikKDev <anikdev2016@gmail.com>
2022-07-14 14:11:54 +0600

    completed experience section primarily

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
 import './Banner.css';
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Experience from './Experience/Experience';
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Experience />
         </div>
     );
 };
